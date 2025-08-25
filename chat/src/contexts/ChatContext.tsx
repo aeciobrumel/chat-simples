bdcd73dee7b8453b5fb83fafd9ca3cfee0271f0e
Aécio Brumel <88293649+aeciobrumel@users.noreply.github.com>
2025-08-24 23:23:26 -0300

    inicio

---
+++ chat/src/contexts/ChatContext.tsx
@@ -0,0 +1,23 @@
+import { createContext, useReducer, type ReactNode } from "react";
+import type { Message } from "../types/Message";
+import { ChatReducer } from "../reducers/chatReducer";
+
+type ChatContext = {
+    chat: Message[];
+    addMessage: (user: string, tect: string) => void
+}
+export const ChatContext = createContext<ChatContext | null>(null);
+
+export const ChatProvider = ({ children }: { children: ReactNode }) => {
+    const [chat, dispatch] = useReducer(ChatReducer, []);
+
+    const addMessage = (user: string, text: string) => {
+        dispatch({
+            type: 'add',
+            payload: { user, text }
+        });
+    }
+    return (
+        <ChatContext.Provider value={{ chat, addMessage }}>{children}</ChatContext.Provider>
+    );
+}
---
+++ chat/src/reducers/chatReducer.ts
@@ -0,0 +1,25 @@
+import type { Message } from "../types/Message";
+
+type  AddAction = {
+    type : 'add';
+    payload: {
+        user:string;
+        text:string;
+    }
+}
+
+type ChatActions = AddAction;
+
+export const ChatReducer =(state: Message[], action : ChatActions) => {
+    switch(action.type){
+        case 'add':
+            return[...state,{
+                id: state.length,
+                user: action.payload.user,
+                text: action.payload.text
+            }];
+        default:
+        return state;
+    }
+
+}
